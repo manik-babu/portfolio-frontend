ed52a3e6fcc1af547c7552b454d19214e30a0d32
manik-babu <mdmanikbabu48@gmail.com>
2026-08-17 23:36:08 +0600

    style: page title and description for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Manik Babu",
   description: "I'm a Full Stack Web Developer who enjoys building modern, scalable, and user focused web applications. I specialize in creating responsive frontends with React and Next.js while developing secure, high-performance backend systems using Node.js, Express.js, PostgreSQL, and MongoDB. I enjoy solving complex problems, writing clean and maintainable code, and continuously learning new technologies to improve my skills. My focus is on building reliable applications that deliver great user experiences while following industry best practices. When I'm not coding, I spend time exploring new technologies, improving my problem solving skills, and expanding my knowledge of software engineering and system design.",
+  openGraph: {
+    title: "Manik Babu",
+    description: "I'm a Full Stack Web Developer who enjoys building modern, scalable, and user focused web applications. I specialize in creating responsive frontends with React and Next.js while developing secure, high-performance backend systems using Node.js, Express.js, PostgreSQL, and MongoDB. I enjoy solving complex problems, writing clean and maintainable code, and continuously learning new technologies to improve my skills. My focus is on building reliable applications that deliver great user experiences while following industry best practices. When I'm not coding, I spend time exploring new technologies, improving my problem solving skills, and expanding my knowledge of software engineering and system design.",
+  },
 };
 
 export default function RootLayout({
